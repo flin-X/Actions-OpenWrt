e73f0dbae593be3afe1db85b9875c681af98d063
superX <2784653935@qq.com>
2020-01-25 23:31:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,5 +8,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+#modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#update luci-theme-argon
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
