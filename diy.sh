f246d173c4fa293ddd2d76f010acfe0a1eb876b2
superX <2784653935@qq.com>
2020-01-26 11:43:31 +0800

    Modify default ssid

--- diy.sh
+++ diy.sh
@@ -9,6 +9,9 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 echo "Modify default IP"
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+#Modify default ssid
+echo "Modify default ssid"
+sed -i 's/OpenWrt/BOOT/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #Modify default theme
 echo "Modify default theme"
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
