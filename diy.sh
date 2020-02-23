46e6b96b33730b95a7905b30db07061147f51765
superX <2784653935@qq.com>
2020-02-23 12:19:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -25,4 +25,4 @@ sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 #Update luci-theme-argon
 echo "Update luci-theme-argon"
 rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/flin-X/luci-theme-argon.git package/lean/luci-theme-argon
